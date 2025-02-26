f3400b11e3006a711b34dc1e08fa27330988e123
YasefDogan <91225382+YasefDogan@users.noreply.github.com>
2025-02-26 15:46:31 +0300

    Update and rename example_config.js to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Mapbox token için ayrı bir konfigürasyon dosyası
+// kullanmak için
+
+
+//kendı api keyinizi girin
+mapboxgl.accessToken = 'YOUR API KEY';
--- example_config.js
+++
@@ -1,6 +0,0 @@
-// Mapbox token için ayrı bir konfigürasyon dosyası
-// kullanmak için
-// bu dosyanın adinı config.js olarak degıstırın
-//yenı bir .env klasoru olusturun
-//kendı api jeyinizi girdikten sonra .env klasorune atarak kullanabilirsiniz
-mapboxgl.accessToken = 'YOUR API KEY';
