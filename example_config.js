553ab468f718b607d0f0a71c1b4d6b463b114e54
YasefDogan <91225382+YasefDogan@users.noreply.github.com>
2025-02-26 09:55:31 +0300

    Update example_config.js

--- example_config.js
+++ example_config.js
@@ -1,3 +1,6 @@
 // Mapbox token için ayrı bir konfigürasyon dosyası
 // Bu dosyayı .gitignore'a ekleyerek güvenli tutabilirsiniz
+// bu dosyanın adinı config.js olarak degıstırın
+//yenı bir .env klasoru olusturun
+//kendı api jeyinizi girdikten sonra .env klasorune atarak kullanabilirsiniz
 mapboxgl.accessToken = 'YOUR API KEY';
